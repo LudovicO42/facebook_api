471b803dd4ce61d682ef9ab456c2277881295c2a
LudovicO42 <ludo42@DESKTOP-04R3NAO.localdomain>
2025-10-31 09:36:56 +0100

    Initial commit - Début du projet Facebook Event API

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+client = MongoClient("mongodb+srv://ludovicolivier_db_user_2:cKR2uV9fulZe4xJ6@cluster0.civfxja.mongodb.net/DB_API_v1")
+db = client["facebook_api"]
+
+app = FastAPI(title="Facebook Event API")
+
+class User(BaseModel):
+    prenom: str
+    nom: str
+    email: EmailStr
+    password: str
+    photo_profil: Optional[str] = None
+    date_inscription: Optional[datetime] = datetime.utcnow()
+
+@app.post("/users")
+def create_user(user: User):
+    if db.users.find_one({"email": user.email}):
+        raise HTTPException(status_code=400, detail="Email déjà utilisé")
+    data = user.dict()
+    db.users.insert_one(data)
+    return {"message": "Utilisateur créé avec succès", "data": data}
+
+@app.get("/users")
+def get_users():
+    users = list(db.users.find())
+    for u in users:
+        u["_id"] = str(u["_id"])
+    return {"users": users}
+
+@app.get("/")
+def root():
+    return {"message": "Facebook Event API - en développement"}
